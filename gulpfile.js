2504cb23c691dc5730e11e2cae2b7a3a9e48313f
U-CS-SHAHRUL-PC\SHAHRUL <hottincup@gmail.com>
2017-04-25 10:35:12 +0800

    added browser headless unit test

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-var livereload = require("gulp-livereload");
+var gulp = require('gulp')
+var shell = require('gulp-shell')
+var livereload = require("gulp-livereload")
+var run = require('tape-run')
+var browserify = require('browserify')
 
 gulp.task('main', function () {
   gulp.src('')
     .pipe(shell('npm run dist'))
     .pipe(shell('npm run distcompress'))
     .pipe(shell('npm run jsdoc'))
-    .pipe(livereload());
-});
+    .pipe(livereload())
+})
 gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch('./index.js', ['main']);
-});
+  livereload.listen()
+  gulp.watch('./index.js', ['main'])
+})
 
-gulp.task('default', ['watch']);
+gulp.task('test', function() {
+  browserify(__dirname + '/test/test.js')
+  .bundle()
+  .pipe(run())
+  .pipe(process.stdout)
+})
+
+gulp.task('default', ['test'])
---
+++ test/test.js
@@ -0,0 +1,24 @@
+var test = require('tape')
+
+var keet = require('../')
+
+// var log = console.log.bind(console)
+
+test('Hello World test', function (t) {
+
+	var vDom = document.createElement('div')
+	vDom.setAttribute('id', 'app')
+	document.body.appendChild(vDom)
+
+	var app = new keet
+	app.link('app', 'Hello World')
+
+    t.plan(1)
+    
+    setTimeout(function () {
+
+        var hello = document.getElementById('app').firstChild.nodeValue
+        t.equal(hello, 'Hello World')
+
+    }, 100)
+})
