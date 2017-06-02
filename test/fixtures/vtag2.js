44342365d712aa726ec0e859f607f50ecdc334e3
U-CS-SHAHRUL-PC\SHAHRUL <hottincup@gmail.com>
2017-06-02 16:56:06 +0800

    fix

--- test/fixtures/vtag2.js
+++ test/fixtures/vtag2.js
@@ -1,23 +1,27 @@
 var Keet = require('../../')
 var log = console.log.bind(console)
 
-var init = function() {
+var init = function(cb) {
   var ctx = this
   var keet = function(tag) {
     return new Keet(ctx)
   }
 
+  var fn = function() {
+    cb(1234567890)
+  }
+
   this.app = keet().link('app', '{{test}}')
   this.test = keet().template('div', 'testTag')
     .set('initial')
+    .vDomLoaded(fn)
 
 }
 
-exports.vtag1 = function(t) {
+exports.vdomloaded = function(t) {
   document.getElementById('app').innerHTML = ''
-  var c = new init
-  c.app.compose(function() { 
-      var v = document.getElementById('testTag')
-      t.ok(v.firstChild.nodeValue === 'initial', 'compose not force')
+  var cc = new init(function(n){
+    t.ok(n = 1234567890, 'vdom loaded event')
   })
+  cc.app.compose()
 }
