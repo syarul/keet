0699cfc4044b778f6a6f74816ce1d1f15fddb0d6
syarul <hottincup@gmail.com>
2018-07-25 04:01:12 +0100

    added more comments

--- keet.js
+++ keet.js
@@ -34,10 +34,10 @@ var next = function (i, ele, els) {
     next.apply(this, [ i, ele, els ])
   } else {
     // Once intial render already in place consecutively
-    // watch the object in Components.prototype.base. We 
-    // can add additional object props or we can delete
-    // existing object props, which will reflect in the 
-    // component rendered elements.
+    // watch the object in Components.prototype.base. Add 
+    // additional object props or delete existing object 
+    // props, which will reflect in the component rendered 
+    // elements.
     var watchObject = function (obj) {
       return new Proxy(obj, {
         set: function (target, key, value) {
@@ -70,10 +70,24 @@ var next = function (i, ele, els) {
  *
  * @param {String | arg0[, arg1[, arg2[, ...]]]} arguments - Custom property names
  * i.e using 'checked' for input elements.
+ * Usage :-
+ *    const App extends Keet {
+ *      constructor(...args) {
+ *        super()
+ *        this.args = args
+ *      }
+ *    }
+ *    const app = new App('checked')
+ * for example usage cases see https://github.com/syarul/keet/blob/master/examples/check.js
  */
 function Keet () {
-  this.base = {}
-  // this.args = [].slice.call(arguments) || []
+  // prepare the store for states, this is the internal state-management for the
+  // components. Personally I never get to like state-management in javascript.
+  // The idea might sound devine but you'll stuck in very complicated get-to-master
+  // this framework/flow cycles where you always write the state in some external 
+  // store and write long logics to do small stuffs and they are very slow. On the 
+  // other hand, this internal store is relatively simple, has referance and 
+  // across multiple components able to share the state in any case.
   Object.defineProperty(this, '__stateList__', {
     enumerable: false,
     writable: true
@@ -81,7 +95,7 @@ function Keet () {
 }
 
 Keet.prototype.mount = function (instance) {
-  // Before we begin to parse an instance, do a rundown checks
+  // Before we begin to parse an instance, do a run-down checks
   // to clean up backtick string which usually has line spacing
   if (typeof instance === 'object') {
     Object.keys(instance).map(function (key) {
@@ -94,6 +108,7 @@ Keet.prototype.mount = function (instance) {
   } else if (typeof instance === 'string') {
     instance = instance.trim().replace(/\s+/g, ' ')
   }
+  // we store the pristine instance in Component.base
   this.base = instance
   return this
 }
